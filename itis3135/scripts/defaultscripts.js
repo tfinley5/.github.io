6ce958229a76a82d129d8be3edfbc1ac3e23aa3a
tfinley5 <tfinley5@uncc.edu>
2022-10-11 13:50:59 -0400

    everything

---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,8 @@
+document.write(hello);
+
+function greeting() {
+    var name = document.getElementById("name");
+    var mood = document.getElementById("mood");
+    document.write("The Finley Inc. welcomes you, " + name + "! We're glad you are doing " + mood);
+
+}
